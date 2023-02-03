b819bd4e9828d42efdabda65cf812ad522053bff
nhueng23 <32nhueng@gmail.com>
2023-02-03 22:26:23 +0700

    bot now can recieve message

--- src/main.py
+++ src/main.py
@@ -1,8 +1,9 @@
-import nextcord
+from nextcord import Message, Intents
 from nextcord.ext import commands
 from dotenv import dotenv_values
 
-bot = commands.Bot()
+intents = Intents(messages=True, guilds=True);
+bot = commands.Bot(intents=intents);
 config = dotenv_values("./.env")
 TOKEN = config["TOKEN"]
 
@@ -10,6 +11,9 @@ TOKEN = config["TOKEN"]
 async def on_ready():
     print(f"WoW i actualy work")
 
-
+#bot.event
+@bot.event
+async def on_message(msg: Message):
+    print(f"cutor {msg.author.name}" + msg.content);
 
 bot.run(TOKEN)
