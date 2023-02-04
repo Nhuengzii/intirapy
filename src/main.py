b36fec1c0935c163dc064120ff4df85263f639af
nhueng23 <32nhueng@gmail.com>
2023-02-04 14:45:11 +0700

    Bot now can recieve sql statement and query from database

--- src/main.py
+++ src/main.py
@@ -1,19 +1,37 @@
 from nextcord import Message, Intents
 from nextcord.ext import commands
 from dotenv import dotenv_values
+import aiopg
 
-intents = Intents(messages=True, guilds=True);
+dsn = "dbname=tdb user=postgres password=passwd host=127.0.0.1"
+
+intents = Intents(messages=True, message_content = True, guilds=True);
 bot = commands.Bot(intents=intents);
 config = dotenv_values("./.env")
 TOKEN = config["TOKEN"]
+
+async def go(state_ment):
+    async with aiopg.create_pool(dsn) as pool:
+        async with pool.acquire() as conn:
+            async with conn.cursor() as cur:
+                if(state_ment == ""):
+                    return
+                await cur.execute(state_ment)
+                ret = []
+                async for row in cur:
+                    ret.append(row)
+                return ret
+
 
 @bot.event
 async def on_ready():
     print(f"WoW i actualy work")
 
-#bot.event
 @bot.event
 async def on_message(msg: Message):
-    print(f"cutor {msg.author.name}" + msg.content);
+    if(msg.author.bot):
+        return
+    result = await go(msg.content);
+    await msg.reply(str(result));
 
 bot.run(TOKEN)
